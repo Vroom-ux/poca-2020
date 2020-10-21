4d11b491261b5c1cefd3974efe84058d69f80c26
aup <aup@informatique.univ-paris-diderot.fr>
2020-10-21 19:27:43 +0200

    add Products.scala

---
+++ src/main/scala/Products.scala
@@ -0,0 +1,22 @@
+package poca
+
+import scala.concurrent.Future
+import slick.jdbc.PostgresProfile.api._
+import java.util.UUID
+
+case class Product(productId: String, productName: String)
+
+final case class ProductAlreadyExistsException(private val message: String="", private val cause: Throwable=None.orNull)
+  extends Exception(message, cause)
+
+class Products {
+
+  class Productstable(tag: Tag) extends Table[(String, String)](tag, "products") {
+    def productId = column[String]("productId", O.PrimaryKey)
+
+    def productname = column[String]("productname")
+
+    def * = (productId, productname)
+  }
+
+}
