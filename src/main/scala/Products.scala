1ca2dd14b1799e2e4d66cd5505e3ed7146ca49d9
luluu12 <fezzoualycia1212@gmail.com>
2020-10-22 01:06:48 +0200

    back

--- src/main/scala/Products.scala
+++ src/main/scala/Products.scala
@@ -5,12 +5,9 @@ import slick.jdbc.PostgresProfile.api._
 import java.util.UUID
 
 case class Product(productId: String, productName: String)
-case class ProductProperties(prix: Float, matiere: String, dimension:Float)
 
 final case class ProductAlreadyExistsException(private val message: String="", private val cause: Throwable=None.orNull)
   extends Exception(message, cause)
-final case class ProductPropertiesAlreadyExistsException(private val message: String="", private val cause: Throwable=None.orNull)
-    extends Exception(message, cause) 
 
 
 class Products {
@@ -25,15 +22,4 @@ class Products {
 
 }
 
-class ProductProperties {
 
-  class Productstable(tag: Tag) extends Table[(Float, String,Float)](tag, "ProductProperties") {
-
-    def productname = column[Float]("prix")
-    def productname = column[String]("matiere")
-    def productname = column[Float]("dimension")
-
-    def * = (prix, matiere,dimension)
-  }
-
-}
