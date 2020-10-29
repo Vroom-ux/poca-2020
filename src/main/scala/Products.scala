41b6cafeb4873a35b670451292db049a2b28b1d5
aup <aup@informatique.univ-paris-diderot.fr>
2020-10-29 13:09:10 +0100

    modif products.scala and Migration01CreateTables.scala

--- src/main/scala/Products.scala
+++ src/main/scala/Products.scala
@@ -4,7 +4,7 @@ import scala.concurrent.Future
 import slick.jdbc.PostgresProfile.api._
 import java.util.UUID
 
-case class Product(productId: String, productName: String)
+case class Product(productId: String, productname: String)
 
 final case class ProductAlreadyExistsException(private val message: String="", private val cause: Throwable=None.orNull)
   extends Exception(message, cause)
@@ -12,7 +12,7 @@ final case class ProductAlreadyExistsException(private val message: String="", p
 
 class Products {
 
-  class Productstable(tag: Tag) extends Table[(String, String)](tag, "products") {
+  class ProductsTable(tag: Tag) extends Table[(String, String)](tag, "products") {
     def productId = column[String]("productId", O.PrimaryKey)
 
     def productname = column[String]("productname")
@@ -20,6 +20,51 @@ class Products {
     def * = (productId, productname)
   }
 
+    implicit val executionContext = scala.concurrent.ExecutionContext.Implicits.global
+    val db = MyDatabase.db
+    val products = TableQuery[ProductsTable]
+
+    def createProduct(productname: String): Future[Unit] = {
+        val existingProductsFuture = getProductByProductname(productname)
+
+        existingProductsFuture .flatMap(existingProducts => {
+            if (existingProducts.isEmpty) {
+                val productId = UUID.randomUUID.toString()
+                val newProduct = Product(productId=productId, productname=productname)
+                val newProductAsTuple: (String, String) = Product.unapply(newProduct).get
+
+                val dbio: DBIO[Int] = products += newProductAsTuple
+                var resultFuture: Future[Int] = db.run(dbio)
+
+                // We do not care about the Int value
+                resultFuture.map(_ => ())
+            } else {
+                throw new ProductAlreadyExistsException(s"A product with productname '$productname' already exists.")
+            }
+        })
+    }
+
+    def getProductByProductname(productname: String): Future[Option[Product]] = {
+        val query = products.filter(_.productname === productname)
+
+        val productListFuture = db.run(query.result)
+
+        productListFuture.map((productList: Seq[(String, String)]) => {
+            productList.length match {
+                case 0 => None
+                case 1 => Some(Product tupled productList.head)
+                case _ => throw new InconsistentStateException(s"Productname $productname is linked to several products in database!")
+            }
+        })
+    }
+
+    def getAllProducts(): Future[Seq[Product]] = {
+        val productListFuture = db.run(products.result)
+
+        productListFuture.map((productList: Seq[(String, String)]) => {
+            productList.map(Product tupled _)
+        })
+    }
 }
 
 
