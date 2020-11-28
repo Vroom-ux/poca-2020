93caa9f6bf04e7b612ea80623f5046acdd0f4957
luluu12 <fezzoualycia1212@gmail.com>
2020-11-28 22:45:00 +0100

    add getproductdetails

--- src/main/scala/Routes.scala
+++ src/main/scala/Routes.scala
@@ -227,6 +227,16 @@ class Routes(users: Users, products : Products, categories : Categories) extends
             }
         }}
 
+   def getproductdetails(fields: Map[String, String]) : Future[HtmlFormat.Appendable] = {
+        logger.info("I got a request to get details of product.")
+    
+        fields.get("productId") match {
+            case Some(productId) => {
+                
+                    val productOPTfuture: Future[Option[Product]] = products.getProductByProductId(productId)
+                    productOPTfuture.map(productOPT => html.productdetails(productOPT))
+}}}
+
     val routes: Route = 
         concat(
             path("hello") {
